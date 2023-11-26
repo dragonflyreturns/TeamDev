6330e24a694b93a7ae533f4dc84a8d91d7ed4d85
Shunsuke024 <shunsuke44440416@gmail.com>
2023-11-26 17:10:03 +0900

    adminlayout

--- resources/views/posts/admin.blade.php
+++ resources/views/posts/admin.blade.php
@@ -1,29 +1,30 @@
 <x-app-layout>
-    
-        <h1>朝楽テーマブログ</h1>
-        <h2>管理者</h2>
-        <form action="/admin" method="POST">
-                @csrf
-                @foreach ($theme as $theme)
-                <div>
-                        
-                        <label>
-                                <input type='checkbox' id='{{$theme->id}}' class='theme' name='theme' value={{$theme->id}}>
-                                {{$theme->theme}}
-                        </label>
-                </div>
-                @endforeach
-                <input type='submit' value="送信">
-        </form>
+    <div class="text-center">
+                <h1 class="text-5xl text-blue-400 lg:font-bold">朝楽テーマブログ</h1>
+                <h2 class="text-xl">管理者画面</h2>
+                <form action="/admin" method="POST">
+                        @csrf
+                        @foreach ($theme as $theme)
+                        <div>
+                                
+                                <label class="text-2xl">
+                                        <input type='checkbox' id='{{$theme->id}}' class='theme' name='theme' value={{$theme->id}}>
+                                        {{$theme->theme}}
+                                </label>
+                        </div>
+                        @endforeach
+                        <input class=" border border-black  bg-white hover:bg-gray-200 text-black rounded px-2 py-1" type='submit' value="送信">
+                </form>
+        </div>
         <script>
                const inputClass = document.getElementsByClassName("theme");
                for(let i=0;i<inputClass.length;i++){
-　　                    inputClass[i].onclick = function(){
+        　　                    inputClass[i].onclick = function(){
                                 for(let u=0;u<inputClass.length;u++){
                                         inputClass[u].checked = false;
                                         this.checked = true;
                                 }
-  　                    }
+        　                    }
                 } 
         </script>
 
